810c434750fdea40416e08cbedbac931f6a3dcd2
Nicolas S <nicolasprodmac@gmail.com>
2024-09-09 23:05:48 -0300

    feat: cooked

--- ds_configuration.js
+++ ds_configuration.js
@@ -1,22 +1,20 @@
-
 exports.config = {
-    dsClientId: process.env.DS_CLIENT_ID ||"8baa1a29-df63-42c8-a2c3-48221dfd0bf3"
-  , dsClientSecret: process.env.DS_CLIENT_SECRET || "bd7c0009-f648-4adc-ad7f-174c628400a7",
-    dsAcountID : "a390f5d4-71f1-4da3-a55d-06939e4521d6"
-  , appUrl: process.env.DS_APP_URL || 'http://localhost:3000' // The url of the application.
-  , production: false
-  , debug: true 
-  , sessionSecret: process.env.SESSION_SECRET ||'12345' 
-  , tokenSecret :  process.env.TOKEN_SECRET || 'LJHDJAS67567%7677SDKLKJSL'
-  , allowSilentAuthentication: true // a user can be silently authenticated if they have an
+  dsClientId: "4109451d-9c5a-41de-a712-e066afac9ba9",
+  dsClientSecret: "a275097f-4613-4e1e-bea8-0d571165bbb6",
+  dsAcountID: "146ce1e2-e653-4262-bfa1-f68eea719bed",
+  appUrl: process.env.DS_APP_URL || "http://localhost:3000/", // The url of the application.
+  production: false,
+  debug: true,
+  sessionSecret: process.env.SESSION_SECRET || "12345",
+  tokenSecret: process.env.TOKEN_SECRET || "LJHDJAS67567%7677SDKLKJSL",
+  allowSilentAuthentication: true, // a user can be silently authenticated if they have an
 
+  targetAccountId: null, // Set if you want a specific DocuSign AccountId, If null, the user's default account will be used.
+};
 
-  , targetAccountId: null // Set if you want a specific DocuSign AccountId, If null, the user's default account will be used.
+exports.config.dsOauthServer = "https://account-d.docusign.com";
 
-}
-
-exports.config.dsOauthServer = exports.config.production ?
-    'https://account.docusign.com' : 'https://account-d.docusign.com';
-
-exports.config.refreshTokenFile =  require('path').resolve(__dirname,'./refreshTokenFile');
-
+exports.config.refreshTokenFile = require("path").resolve(
+  __dirname,
+  "./refreshTokenFile"
+);
--- embedded_signing.js
+++ embedded_signing.js
@@ -1,121 +1,123 @@
 const docusign = require("docusign-esign");
 const fs = require("fs");
 
-
-const sendEnvelopeUsingEmbeddedSending = async (req,res) => {
-    
-    var args={
-      document_title :'This is demonstration PDF',
-      document_description:'The description always helps people to understand the content of this documents',
-        startingView: 'tagging',
-        envelopeArgs: {
-            signerEmail: ['krutika.bhatt@somaiya.edu','krutikabhatt222@gmail.com'],
-            signerName: ['Krutika Bhatt','Diamonds Shine'],
-            dsReturnUrl: 'http://localhost:3000/ds-return',
-            doc2File: './sample-pdf.pdf'
-        }
-    }
-    
-    console.log(args);
-    args.accessToken = req.cookies['access-token'];
-    args.accountId = "a390f5d4-71f1-4da3-a55d-06939e4521d6";
-    args.basePath= 'https://demo.docusign.net/restapi';
-    console.log("Args :",args);
-    console.log("Hello I m in send Envelope",args);
-  
-    let dsApiClient = new docusign.ApiClient();
-    dsApiClient.setBasePath(args.basePath);
-    dsApiClient.addDefaultHeader("Authorization", "Bearer " + args.accessToken);
-    let envelopesApi = new docusign.EnvelopesApi(dsApiClient);
-  
-    args.envelopeArgs.status = "created"; 
-  
-    let envelope = makeEnvelope(args.envelopeArgs);
-  
-    let results = await envelopesApi.createEnvelope(args.accountId, {
-      envelopeDefinition: envelope,
-    })
-    .catch(err =>{
-      console.log(err);
-      return res.status(400).send(err);
-    })
-    let envelopeId = results.envelopeId;
-
-    console.log(envelopeId);
-    let viewRequest = makeSenderViewRequest(args.envelopeArgs);
-    console.log("This make Sender Request is done");
-    results = await envelopesApi.createSenderView(args.accountId, envelopeId, {
-      returnUrlRequest: viewRequest,
-    });
-
-    console.log("In results log");
-    
-    let url = results.url;
-    console.log(`startingView: ${args.startingView}`);
-    if (args.startingView === "recipient") {
-      url = url.replace("send=1", "send=0");
-    }
-  
-    return res.status(200).json({ envelopeId: envelopeId, redirectUrl: url });
+const sendEnvelopeUsingEmbeddedSending = async (req, res) => {
+  var args = {
+    document_title: "This is demonstration PDF",
+    document_description:
+      "The description always helps people to understand the content of this documents",
+    startingView: "tagging",
+    envelopeArgs: {
+      signerEmail: ["krutika.bhatt@somaiya.edu", "krutikabhatt222@gmail.com"],
+      signerName: ["Krutika Bhatt", "Diamonds Shine"],
+      dsReturnUrl: "http://localhost:3000/ds-return",
+      doc2File: "./sample-pdf.pdf",
+    },
   };
 
-  function makeSenderViewRequest(args) {
-    let viewRequest = new docusign.ReturnUrlRequest();
-  
-    viewRequest.returnUrl = args.dsReturnUrl;
-    return viewRequest;
-  }
-  
-  function makeEnvelope(args) {
-  
-    let doc2DocxBytes, doc3PdfBytes;
-  
-    doc2DocxBytes = fs.readFileSync(args.doc2File);
- 
-    let env = new docusign.EnvelopeDefinition();
-    env.emailSubject = "Please sign this document set";
-  
-    var doc2b64 = Buffer.from(doc2DocxBytes).toString("base64");
-    let doc2 = new docusign.Document.constructFromObject({
-      documentBase64: doc2b64,
-      name: args.document_title, 
-      fileExtension: "pdf",
-      documentId: "1",
+  console.log(args);
+  args.accessToken = req.cookies["access-token"];
+  args.accountId = "47d930d2-005a-446c-ad5d-49b007aa15d4";
+  args.basePath = "https://demo.docusign.net/restapi";
+  console.log("Args :", args);
+  console.log("Hello I m in send Envelope", args);
+
+  let dsApiClient = new docusign.ApiClient();
+  dsApiClient.setBasePath(args.basePath);
+  dsApiClient.addDefaultHeader("Authorization", "Bearer " + args.accessToken);
+  let envelopesApi = new docusign.EnvelopesApi(dsApiClient);
+
+  args.envelopeArgs.status = "created";
+
+  let envelope = makeEnvelope(args.envelopeArgs);
+
+  let results = await envelopesApi
+    .getEnvelope(
+      "47d930d2-005a-446c-ad5d-49b007aa15d4",
+      "ee9830ba-90f0-4871-8712-58c3a9db78a6"
+    )
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).send(err);
     });
-  
-    env.documents = [doc2];
- 
-    var Allsigners =[];
-    var signerEmail_list = args.signerEmail;
-    var signerName_list = args.signerName;
-    var recIDs = 1;
-    for(var i=0;i<signerEmail_list.length;i++){
-        var signer1 = docusign.Signer.constructFromObject({
-            email: signerEmail_list[i],
-            name: signerName_list[i],
-            recipientId: recIDs.toString(),
-            routingOrder: recIDs.toString(),
-        });
-        Allsigners.push(signer1);
-        recIDs = parseInt(recIDs) + 1;
-    }
-   
-    console.log("All signers :",Allsigners);
-    let recipients = docusign.Recipients.constructFromObject({
-      signers: Allsigners
-    });
-    env.recipients = recipients;
-  
-    env.status = args.status;
-  
-    return env;
+  let envelopeId = results.envelopeId;
+
+  let a = await envelopesApi.createEnvelope(args.accountId);
+  console.log(a);
+
+  console.log(results, "results");
+
+  console.log(envelopeId);
+  let viewRequest = makeSenderViewRequest(args.envelopeArgs);
+  console.log("This make Sender Request is done");
+  results = await envelopesApi.createSenderView(args.accountId, envelopeId, {
+    returnUrlRequest: viewRequest,
+  });
+
+  console.log("In results log");
+
+  let url = results.url;
+  console.log(`startingView: ${args.startingView}`);
+  if (args.startingView === "recipient") {
+    url = url.replace("send=1", "send=0");
   }
-  
-  
-  function document1(args) {
- 
-  
-    return `
+
+  return res.status(200).json({ envelopeId: envelopeId, redirectUrl: url });
+};
+
+function makeSenderViewRequest(args) {
+  let viewRequest = new docusign.ReturnUrlRequest();
+
+  viewRequest.returnUrl = args.dsReturnUrl;
+  return viewRequest;
+}
+
+function makeEnvelope(args) {
+  let doc2DocxBytes, doc3PdfBytes;
+
+  doc2DocxBytes = fs.readFileSync(args.doc2File);
+
+  let env = new docusign.EnvelopeDefinition();
+  env.emailSubject = "Please sign this document set";
+
+  var doc2b64 = Buffer.from(doc2DocxBytes).toString("base64");
+  let doc2 = new docusign.Document.constructFromObject({
+    documentBase64: doc2b64,
+    name: args.document_title,
+    fileExtension: "pdf",
+    documentId: "1",
+  });
+
+  env.documents = [doc2];
+
+  var Allsigners = [];
+  var signerEmail_list = args.signerEmail;
+  var signerName_list = args.signerName;
+  var recIDs = 1;
+  for (var i = 0; i < signerEmail_list.length; i++) {
+    var signer1 = docusign.Signer.constructFromObject({
+      email: signerEmail_list[i],
+      name: signerName_list[i],
+      recipientId: recIDs.toString(),
+      routingOrder: recIDs.toString(),
+    });
+    Allsigners.push(signer1);
+    recIDs = parseInt(recIDs) + 1;
+  }
+
+  console.log("All signers :", Allsigners);
+  let recipients = docusign.Recipients.constructFromObject({
+    signers: Allsigners,
+  });
+  env.recipients = recipients;
+
+  env.status = args.status;
+
+  return env;
+}
+
+function document1(args) {
+  return `
       <!DOCTYPE html>
       <html>
           <head>
@@ -136,8 +138,6 @@ const sendEnvelopeUsingEmbeddedSending = async (req,res) => {
           </body>
       </html>
     `;
-  }  
-
-
+}
 
 module.exports = { sendEnvelopeUsingEmbeddedSending };
